ce553c3a0ef554a4ec3bea3ed336ebe862247a17
Mohamed Ammar <ammarmohamed074@gmail.com>
2020-07-28 19:06:16 +0200

    Second Edit

--- js/main.js
+++ js/main.js
@@ -8,21 +8,46 @@ $(window).on('load', function() {
 })
 
 $(document).ready(function() {
-
+    //this is to open menu in small screens
     $(".sign-up .hidden-menu").click(function() {
         $("body").toggleClass("overflow")
         $(".small-hidden-list .white-list").toggleClass("overflow");
         $(".small-hidden-list").fadeToggle(600);
     });
 
-    $(".nested-list button").click(function() {
+    //--------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    //this is to open Search in small screens
+    $(".sign-up .search-button").click(function() {
+        $("body").toggleClass("overflow")
+        $(".sign-up .search-button").toggleClass("overflow");
+        $(".small-search").fadeToggle(600);
+    });
+
+    //this is to open big list in menu in small screens
+    $(".nested-list span").click(function() {
         $("div.white-list").toggleClass("display-overflow");
         $(".nested-list .nested-list-icon").toggleClass("rotate");
         $(".nav-small-list-one").fadeToggle(600);
     });
+    //this is to open sub list in menu in small screens
     $(".nested-list-two").click(function() {
         $(".nested-list .nested-list-two-icon").toggleClass("rotate");
         $(".nav-small-list-two").fadeToggle(600);
+    });
+
+    $(".colno2 h4").click(function() {
+        $(" .colno2 .footer-data-list").slideToggle(600);
+    });
+    $(".colno3 h4").click(function() {
+        $(" .colno3 .footer-data-list").slideToggle(600);
+    });
+    $(".colno4 h4").click(function() {
+        $(" .colno4 .footer-data-list").slideToggle(600);
+    });
+    $(".colno5 h4").click(function() {
+        $(" .colno5 .footer-data-list").slideToggle(600);
     });
 
     /*$(".parent").hover(function() {
@@ -50,17 +75,22 @@ $(document).ready(function() {
                 nav: false,
                 loop: true
             },
-            600: {
+            768: {
                 items: 1,
                 nav: true,
                 loop: true
             },
-            1000: {
+            992: {
                 items: 1,
                 nav: true,
                 loop: true
             },
-            1350: {
+            1200: {
+                items: 1,
+                nav: true,
+                loop: true
+            },
+            1400: {
                 items: 1,
                 nav: true,
                 loop: true
@@ -82,12 +112,12 @@ $(document).ready(function() {
             },
             768: {
                 items: 3,
-                nav: false,
+                nav: true,
                 loop: true
             },
             992: {
                 items: 4,
-                nav: false,
+                nav: true,
                 dots: true,
                 loop: true
             },
@@ -127,7 +157,7 @@ $(document).ready(function() {
             },
             992: {
                 items: 4,
-                nav: true,
+                nav: false,
                 dots: true,
                 loop: true
             },
